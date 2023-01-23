c352d9bae382d768cecac133b2a2d04f0e00db16
VineethDShetty <vineethshetty289@gmail.com>
2023-01-23 19:01:18 +0530

    Update BubbleChart.jsx

--- react-charts/src/Components/BubbleChart.jsx
+++ react-charts/src/Components/BubbleChart.jsx
@@ -9,17 +9,35 @@ function BubbleChart() {
       datasets: [{
         label: 'First Dataset',
         data: [{
-          x: 20,
-          y: 30,
+          x: 34,
+          y: 15,
           r: 15
         }, {
           x: 40,
           y: 10,
           r: 10
         },{
-          x:20,
-          y:0,
+          x:22,
+          y:4,
           r:5
+        },
+        {
+          x: 25,
+          y: 10,
+          r: 30
+        },{
+          x:20,
+          y:10,
+          r:5
+        },
+        {
+          x: 25,
+          y: 30,
+          r: 10
+        },{
+          x:20,
+          y:20,
+          r:25
         }
       ],
         backgroundColor: 'rgb(255, 99, 132)'
